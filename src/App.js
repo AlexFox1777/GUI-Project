32c3e77ac24f9802fc19fdc8a1b736e89a09e79e
Sasha <mix.filipov@mail.ru>
2019-08-21 21:01:22 -0700

    First update

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
           target="_blank"
           rel="noreferrer noopener"
         >
-          This project
+          This My project
         </a>{' '}
         was bootstrapped with{' '}
         <a href="https://facebook.github.io/create-react-app/">
